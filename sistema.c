c88309fdc9f5a6e0c69498c27be9457f04da33ac
Agustin Maximiliano Stoppello <121363716+Agustinstpp@users.noreply.github.com>
2025-10-28 15:30:55 -0300

    Actualizar sistema.c

---
+++ sistema.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stbool.h>
+#include <string.h>
+
+/* Parte TAD */
+
+//Cola 
+typedef struct NodoCola {
+    int idUsuario; //
+    struct NodoCola* siguiente;
+} NodoCola;
+
+typedef struct {
+    NodoCola *frente, *final;
+} Cola;
+
+//Libros
+typedef struct {
+    long long code;
+    char titulo[100];
+    char autor[100];
+    int disponible; // 1 = disponible, 0 = prestado
+    Cola* listaEspera; // Cola de IDs de usuarios esperando por este libro
+} Libro;
